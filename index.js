6d8e148c19c64ab8b739151e885fe02eadefa8f3
Ironious8 <tyronekariuki@gmail.com>
2024-07-21 19:53:12 +0300

    Add fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = `ID: ${id}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      const errorElement = document.createElement("p");
+      errorElement.textContent = `Error: ${errorMessage}`;
+      document.body.appendChild(errorElement);
+    });
+  }
+  
